8b341c8ebb64afbfb74134d580f1785530a86209
Mo Ramezanpoor <me@mohsenr.com>
2019-08-26 13:41:31 +0100

    Create initialiser for Amount that preserves the correct number of fraction digits.

--- Sources/ForeignExchange/Model/Amount.swift
+++ Sources/ForeignExchange/Model/Amount.swift
@@ -1,5 +1,7 @@
 import Foundation
+import Support
 
+/// A monetary amount in a given currency.
 public struct Amount: Equatable {
     public var value: Decimal
     public var currency: Currency
@@ -12,11 +14,43 @@ public struct Amount: Equatable {
 
 extension Amount {
     
-    public init(value: Double, significantFractionDigits: Int, in currency: Currency) {
+    /// Creates an amount with the specified value and currency.
+    ///
+    /// The amount only retains the significant digits expected for the specified currency.
+    ///
+    /// - Parameter value: The value of the amount.
+    /// - Parameter currency: The currency of the amount.
+    public init(value: Double, in currency: Currency) {
+        let significantFractionDigits = Self.standardSignificantFractionDigits(for: currency)
         self.init(
             value: Decimal(value: value, significantFractionDigits: significantFractionDigits),
             currency: currency
         )
+    }
+    
+}
+
+private extension Amount {
+    
+    static var sema = DispatchSemaphore(value: 1)
+    static var significantDigits = [Currency: Int]()
+    static let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyISOCode
+        return formatter
+    }()
+    static func standardSignificantFractionDigits(for currency: Currency) -> Int {
+        sema.wait()
+        defer { sema.signal() }
+        
+        if let digits = significantDigits[currency] {
+            return digits
+        } else {
+            formatter.currencyCode = currency.code
+            let digits = formatter.maximumFractionDigits
+            significantDigits[currency] = digits
+            return digits
+        }
     }
     
 }
--- Tests/ForeignExchangeTests/Model/AmountTests.swift
+++ Tests/ForeignExchangeTests/Model/AmountTests.swift
@@ -3,10 +3,15 @@ import XCTest
 
 class AmountTests: XCTestCase {
     
-    func testThatInitWithDoubleAndNoFractionDigits() {
-        let actual = Amount(value: 1.234, significantFractionDigits: 0, in: .gbp)
-        let expected = Amount(value: Decimal(integerLiteral: 1), currency: .gbp)
-        
+    func testThatGBPAmountsKeepsTwoFractionDigits() {
+        let actual = Amount(value: 1.234, in: .gbp).value
+        let expected = Decimal(value: 1.234, significantFractionDigits: 2)
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testThatJPYAmountsKeepsZeroFractionDigits() {
+        let actual = Amount(value: 1.234, in: Currency(code: "JPY")).value
+        let expected = Decimal(value: 1, significantFractionDigits: 0)
         XCTAssertEqual(actual, expected)
     }
     
